47b88d96a9e05cb8a34dce690810036f550e026d
riomz07 <riomadaraz7@gmail.com>
2021-08-23 18:16:43 +0700

    joke

---
+++ script.js
@@ -0,0 +1,7 @@
+var nama = prompt("Siapa Namamu ?");
+
+if (nama == "Della" || nama == "DELLA" || nama == "della") {
+  alert("Selamat datang manisss !!!, Love youuuu ( ˘ ³˘)♥ ");
+} else {
+  alert("Selamat datang !!! ");
+}
